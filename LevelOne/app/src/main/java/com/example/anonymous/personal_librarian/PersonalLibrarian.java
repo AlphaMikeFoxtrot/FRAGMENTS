b612464415584111c1c93ccb64a6afd16b735af1
ANONYMOUS <iamcicada3301@gmail.com>
2017-12-28 18:23:26 +0530

    initial commit

---
+++ LevelOne/app/src/main/java/com/example/anonymous/personal_librarian/PersonalLibrarian.java
@@ -0,0 +1,13 @@
+package com.example.anonymous.personal_librarian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonalLibrarian extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_librarian);
+    }
+}
